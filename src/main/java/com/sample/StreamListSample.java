1a2da826d7995581e34136341f0119a8c30d4b29
RAJ JAISWAL <rajjaiswalsaumya@gmail.com>
2019-03-17 01:50:21 +0530

    Hash inferences understanding what is happening with set

--- src/main/java/com/sample/StreamListSample.java
+++ src/main/java/com/sample/StreamListSample.java
@@ -1,6 +1,7 @@
 package com.sample;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,19 +9,28 @@ public class StreamListSample {
 	public static void main(String[] args) {
 		List l1 = new ArrayList<>();
 
-		l1.add("Raj");
-		l1.add("Vinay");
-		l1.add("Sudipa");
-		l1.add("Mandar");
-		l1.add("Surbhi");
-		l1.add("Mandar");
-		l1.add("Ayaz");
-		l1.add("Mandar");
+//		l1.add("Raj");
+//		l1.add("Vinay");
+//		l1.add("Sudipa");
+//		l1.add("Mandar");
+//		l1.add("Surbhi");
+//		l1.add("Mandar");
+//		l1.add("Ayaz");
+//		l1.add("Mandar");
+
+		l1.add(1);
+		l1.add(2);
+		l1.add(3);
+		l1.add(4);
+		l1.add(5);
+		l1.add(4);
+		l1.add(6);
+		l1.add(4);
 
 		System.out.println(l1);
 
-//		HashSet set = new HashSet(l1);
-//		System.out.println(set);
+		HashSet set = new HashSet(l1);
+		System.out.println(set);
 
 		System.out.println(l1.stream().distinct().collect(Collectors.toList()));
 	}
